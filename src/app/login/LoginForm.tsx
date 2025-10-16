4db15405c18ce1ba60bdbc7026714650d55bd216
rjndev <nebrijaroyjoshua@gmail.com>
2025-10-17 00:07:02 +0800

    Login page redesign

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import { useAuth } from "@/hooks/authHooks";
 import { redirect } from "next/navigation";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import Image from "next/image";
 
 
 
@@ -21,34 +22,43 @@ export default function LoginForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center justify-center min-h-screen">
-      <h1 className="font-bold text-lg">BG Locum Mailer Login</h1>
+    <form onSubmit={handleSubmit} className="flex flex-1 flex-col gap-4 items-center justify-center min-h-screen">
+      <Image className="lg:hidden block" src="/bg-logo.svg" alt="Blugibbon Logo" width={200} height={100} />
+      <h1 className="font-bold text-4xl mt-8 mb-4">Welcome Back!👋</h1>
+      <h2 className="text-lg">Sign in to see your next Locum jobs now!</h2>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 mt-4">
         <span className="flex gap-4 items-center justify-between">
-          <label htmlFor="username" className="font-semibold">Email</label>
+          <label htmlFor="username" className="font-semibold text-lg">Email</label>
           <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" id="username" className="border border-gray-300 rounded-md py-1 px-2 transition-all duration-300 ease-in-out focus:border-blue-500 focus:ring-[1px] focus:ring-blue-500 outline-none" placeholder="sample@mail.com" />
         </span>
 
         <span className="flex gap-4 items-center justify-center">
-          <label htmlFor="password" className="font-semibold">Password</label>
+          <label htmlFor="password" className="font-semibold text-lg">Password</label>
           <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" className="border border-gray-300 rounded-md py-1 px-2 transition-all duration-300 ease-in-out focus:border-blue-500 focus:ring-[1px] focus:ring-blue-500 outline-none" placeholder="••••••••" />
         </span>
       </div>
 
       <p className={`${error ? "text-red-500" : "hidden"}`}>Login failed. Please try again.</p>
 
-      {
-        fetching ?  (<span className="flex gap-2 justify-center items-center"><p>Logging in...</p> <AiOutlineLoading3Quarters className="animate-spin inline-block" size={20} /></span>) : 
+      {/* {
+        fetching ?  (<span className="flex gap-2 mt-6 justify-center items-center"><p>Logging in...</p> <AiOutlineLoading3Quarters className="animate-spin inline-block" size={20} /></span>) : 
         (
         <button
           className="bg-blue-600 text-white rounded-md mt-4 py-1 px-4 font-semibold hover:cursor-pointer hover:bg-blue-500 transition-colors duration-300 ease-in-out disabled:bg-gray-500"
           disabled={email == "" || password == ""}
           type="submit">
-            Login
+            {fetching ? <AiOutlineLoading3Quarters className="animate-spin inline-block" size={20} /> : "Login"}
           </button>
         )
-      }
+      } */}
+
+      <button
+        className="bg-blue-600 flex items-center justify-center text-white rounded-md mt-4 py-1 px-4 w-[72px] h-[36px] font-semibold hover:cursor-pointer hover:bg-blue-500 transition-colors duration-300 ease-in-out disabled:bg-gray-500"
+        disabled={email == "" || password == ""}
+        type="submit">
+          {fetching ? <AiOutlineLoading3Quarters className="animate-spin inline-block" size={20} /> : "Login"}
+      </button>
 
     </form>
   )
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utlis/supabase/supabase-server";
 import LoginForm from "./LoginForm";
+import Image from "next/image";
 
 
 export default async function Login() {
@@ -12,8 +13,17 @@ export default async function Login() {
   }
 
   return (
-    <div>
+    <div className="flex">
       <LoginForm />
+      <div className=" bg-linear-to-r from-cyan-300 to-blue-400  w-full lg:block hidden flex-1 flex-col justify-center items-center">
+        {/* Decorative side - can add image or branding here */}
+
+        <div className="flex items-center justify-center flex-col h-full">
+          <Image className="lg:block hidden" src="/bg-logo.svg" alt="Blugibbon Logo" width={420} height={220} />
+          <h2 className="p-8 text-2xl text-blue-900">Welcome to our Locum Mailer!</h2>
+          <p className="text-blue-900 p-8 text-xl">Please login to continue.</p>
+        </div>
+      </div>
     </div>
   )
 }
